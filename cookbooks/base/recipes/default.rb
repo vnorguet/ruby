b15e59fe3f8f5dd2dcff87accd2ac3a5facafc52
Vincent NORGUET <vnorguet@heliostech.fr>
2014-06-08 12:03:13 +0200

    Add basic install tools

---
+++ cookbooks/base/recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: base
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "nginx"
+package "zsh"
+package "git"
+package "curl"
+package "libmysqlclient-dev"
